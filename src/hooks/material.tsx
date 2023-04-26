bd4e9be09ae9844aad16acdfda550143e1c552d2
chooxingyu <chooxingyu@hotmail.com>
2023-04-26 14:14:38 +0800

    [#3] add get material hooks

---
+++ src/hooks/material.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { QUERY_KEYS } from "../types/constants";
+import { Material } from "../types/types";
+
+export const useMaterialGet = () => {
+  return useQuery({
+    queryKey: QUERY_KEYS.MATERIAL,
+    queryFn: async () =>
+      (await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/material`)).data as Material[],
+  });
+};
