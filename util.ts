59a79fa7b7dbbc838176e2350d08bff2b23d0fb9
chooxingyu <chooxingyu@hotmail.com>
2023-04-27 09:28:11 +0800

    [#3] mult by 100 for per 100g/ml using bigjs

--- util.ts
+++ util.ts
@@ -1,5 +1,7 @@
 import { parseISO, format } from 'date-fns'
 
+import Big, { BigSource } from 'big.js'
+
 export function formatText(str: string) {
     let newString = str
         .replace('id', 'ID')
@@ -34,4 +36,10 @@ export function formatDate(str: string | undefined) {
     if (!str) return '';
     const date = parseISO(str);
     return format(date, 'dd/MM/yy h:mma');
+}
+
+// Nutrition stored in backend is per grams/ml
+// but user interacts with nutrition in per 100g/100ml
+export function formatNutriValue(nutrition: BigSource): string {
+    return Big(nutrition).times(100).toString();
 }
