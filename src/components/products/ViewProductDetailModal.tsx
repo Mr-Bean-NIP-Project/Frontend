7d29d908cee309cc5af1d77434379ae55b441902
Raine <77650951+yuyitan@users.noreply.github.com>
2023-04-29 22:47:40 +0800

    Update ViewProductDetailModal.tsx

--- src/components/products/ViewProductDetailModal.tsx
+++ src/components/products/ViewProductDetailModal.tsx
@@ -1,22 +1,15 @@
-import { Modal, Table, Text, createStyles } from "@mantine/core";
+import {
+  Badge,
+  Box,
+  Group,
+  Modal,
+  Table,
+  Text,
+  createStyles,
+} from "@mantine/core";
 import { ModalStateEnum } from "../../types/constants";
 import { Product } from "../../types/types";
 import DimmedMessage from "../shared/DimmedMessage";
-
-const useStyles = createStyles((theme) => ({
-  subProductTitle: {
-    backgroundColor:
-      theme.colorScheme === "light"
-        ? theme.colors.gray[0]
-        : theme.colors.gray[9],
-  },
-  materialTitle: {
-    backgroundColor:
-      theme.colorScheme === "light"
-        ? theme.colors.gray[0]
-        : theme.colors.gray[9],
-  },
-}));
 
 interface ViewProductDetailModalProps {
   product: Product | undefined;
@@ -24,90 +17,90 @@ interface ViewProductDetailModalProps {
   onClose(): void;
 }
 
+const NAME_WIDTH = "75%";
+
 export const ViewProductDetailModal = ({
   product,
   modalState,
   onClose,
 }: ViewProductDetailModalProps) => {
-  const { classes } = useStyles();
   if (!product) return null;
+
+  const tableHeaders = (
+    <tr>
+      <th>
+        <Text weight={600} color="dark">
+          Name
+        </Text>
+      </th>
+      <th align="right">
+        <Text weight={600} color="dark" align="right">
+          Quantity (in g or ml)
+        </Text>
+      </th>
+    </tr>
+  );
 
   const subProductRows = (product.product_sub_products ?? []).map((psp) => (
     <tr key={psp.id}>
-      <td width={"40%"}>
-        <Text weight={500}>{psp.child.name}</Text>
-      </td>
-      <td align="right">
-        <Text weight={500}>{psp.quantity}</Text>
-      </td>
+      <td width={NAME_WIDTH}>{psp.child.name}</td>
+      <td align="right">{psp.quantity}</td>
     </tr>
   ));
-  const subProductTable = (
-    <>
-      <Text>Sub Products ({subProductRows.length})</Text>
-      <Table
-        withBorder
-        withColumnBorders
-        fontSize="md"
-        horizontalSpacing="md"
-        verticalSpacing="xs"
-      >
-        <colgroup>
-          <col className={classes.subProductTitle} />
-          <col />
-        </colgroup>
-        <tbody>
-          <tr>
-            <td>
-              <Text weight={500}>Name</Text>
-            </td>
-            <td align="right">
-              <Text weight={500}>Quantity</Text>
-            </td>
-          </tr>
-          {subProductRows}
-        </tbody>
-      </Table>
-    </>
-  );
 
   const subMaterialRows = (product.material_product ?? []).map((mp) => (
     <tr key={mp.id}>
-      <td width={"40%"}>
-        <Text weight={500}>{mp.material.name}</Text>
-      </td>
-      <td align="right">
-        <Text weight={500}>{mp.material_quantity}</Text>
-      </td>
+      <td width={NAME_WIDTH}>{mp.material.name}</td>
+      <td align="right">{mp.material_quantity}</td>
     </tr>
   ));
-  const subMaterialTable = (
-    <>
-      <Text>Materials ({subMaterialRows.length})</Text>
+
+  const subProductTable = (
+    <Box style={{ marginBottom: subMaterialRows.length > 0 ? 25 : 10 }}>
+      <Group position="left" align="inherit">
+        <Text weight={600} size="lg" style={{ marginBottom: 10 }}>
+          Sub-Products
+        </Text>
+        <Badge size="lg" style={{ marginLeft: -5 }}>
+          {subProductRows.length}
+        </Badge>
+      </Group>
       <Table
+        striped
         withBorder
         withColumnBorders
         fontSize="md"
         horizontalSpacing="md"
         verticalSpacing="xs"
       >
-        <colgroup>
-          <col className={classes.subProductTitle} />
-          <col />
-        </colgroup>
-        <tbody>
-          <tr>
-            <td>
-              <Text weight={500}>Name</Text>
-            </td>
-            <td align="right">
-              <Text weight={500}>Quantity</Text>
-            </td>
-          </tr>
-          {subMaterialRows}
-        </tbody>
+        <thead>{tableHeaders}</thead>
+        <tbody>{subProductRows}</tbody>
       </Table>
-    </>
+    </Box>
+  );
+
+  const subMaterialTable = (
+    <Box style={{ marginBottom: 10 }}>
+      <Group position="left" align="inherit">
+        <Text weight={600} size="lg" style={{ marginBottom: 10 }}>
+          Materials
+        </Text>
+        <Badge size="lg" style={{ marginLeft: -5 }}>
+          {subMaterialRows.length}
+        </Badge>
+      </Group>
+      <Table
+        striped
+        withBorder
+        withColumnBorders
+        fontSize="md"
+        horizontalSpacing="md"
+        verticalSpacing="xs"
+      >
+        <thead>{tableHeaders}</thead>
+        <tbody>{subMaterialRows}</tbody>
+      </Table>
+    </Box>
   );
 
   const hasNoResult: boolean =
@@ -116,15 +109,15 @@ export const ViewProductDetailModal = ({
   return (
     <>
       <Modal
-        size="lg"
+        size="xl"
         opened={modalState === ModalStateEnum.View}
         onClose={() => onClose()}
         title={`#${product.id}: ${product.name}`}
       >
         {hasNoResult ? (
           <DimmedMessage
-            title={"No SubProducts/Materials tagged!"}
-            subtitle={"Click on the edit button to tag SubProducts/Materials"}
+            title={"No sub-products or materials tagged"}
+            subtitle={"Edit this product to tag sub-products or materials!"}
           />
         ) : null}
         {subProductRows.length > 0 ? subProductTable : null}
