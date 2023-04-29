a8ad6704688999bdd3fd6735928a5a4699f29e58
Raine <77650951+yuyitan@users.noreply.github.com>
2023-04-29 14:00:00 +0800

    [#37] import inter font from google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import SideNavBar from "@/components/shared/SideNavBar";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
@@ -28,28 +29,36 @@ export default function App({ Component, pageProps }: AppProps) {
   );
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          fontFamily: "Inter, sans-serif",
-          primaryColor: "indigo",
-          colorScheme,
-        }}
+    <>
+      <Head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+        </style>
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={pageProps.dehydratedState}>
-            <Notifications />
-            <AppShell navbar={<SideNavBar />}>
-              <Component {...pageProps} />
-            </AppShell>
-          </Hydrate>
-        </QueryClientProvider>
-      </MantineProvider>
-    </ColorSchemeProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            fontFamily: "Poppins, sans-serif",
+            primaryColor: "indigo",
+            colorScheme,
+          }}
+        >
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <Notifications />
+              <AppShell navbar={<SideNavBar />}>
+                <Component {...pageProps} />
+              </AppShell>
+            </Hydrate>
+          </QueryClientProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
   );
 }
